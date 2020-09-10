ea660ff03b2b6b45833069655a9995fbc82ca54a
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-09-10 15:47:53 +0300

    problem 3

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the
+ first element is the sum of those three numbers, the second element is the product of those three numbers, 
+  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
@@ -48,11 +50,23 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    //Third element: "4 and 7 and 5 sum to 16."
+
+   
+    var sum1=sum(a+b,c);
+    var multi2=multiply(a*b,c);
+    var x=a +' and '+ b +' and '+ c+ ' sum to ' +sum1[0] +'.';
+    //Third element: "4 and 7 and 5 sum to 16."
+    var y='The product of '+ a +' and '+ b + ' and '+ c + ' is ' + multi2[0] + '.';
+    var arraysumAndMultiply=[sum1[0],multi2[0],x,y];
+    console.log(arraysumAndMultiply);
+    return arraysumAndMultiply;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
+// sumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
